4d189f5f6af0f15415648ed9db5d18250b1d0417
victormsg29 <119140872+victormsg29@users.noreply.github.com>
2026-01-16 14:31:37 -0500

    Update city.js

--- city.js
+++ city.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const race = localStorage.getItem("race");
+  const city = document.querySelector(".city");
 
+  if (!race) {
+    window.location.href = "../race.html";
+    return;
+  }
+
+  const backgrounds = {
+    humano: "../images/fondo_humano.webp",
+    elfo: "../images/fondo_elfo.webp",
+    orco: "../images/fondo_orco.webp"
+  };
+
+  city.style.backgroundImage = `url(${backgrounds[race]})`;
+});
